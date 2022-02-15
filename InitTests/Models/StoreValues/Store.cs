a4f8e49ec50e419fc584fe1e287d5661a0d48c87
stone1234765 <51057074+stone1234765@users.noreply.github.com>
2022-02-15 21:24:26 +0200

    Foo changes

--- InitTests/Models/StoreValues/Store.cs
+++ InitTests/Models/StoreValues/Store.cs
@@ -6,6 +6,18 @@
         public List<Product> Products { get; set; }
         public Product GetProduct(Guid id)
         {
+            if (Products != null)
+            {
+                Products.Add(new Product());
+                Products.Add(new Product());
+                Products.Add(new Product());
+                Products.Add(new Product());
+                Products.Add(new Product());
+                Products.Add(new Product());
+            }
+            string test = "";
+            Console.WriteLine(test);
+            Products = new();
             return new Product() { Id = id, Name = "For test" };
         }
     }
